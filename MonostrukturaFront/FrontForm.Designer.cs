40d89b4aef6adebfb7fd23c332c37bf583788d0b
Tomasz Czarnecki <creo.one@gmail.com>
2017-04-09 19:38:56 +0200

    Added project as starting point of application.

---
+++ MonostrukturaFront/FrontForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace MonostrukturaFront
+{
+    partial class FrontForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bMonostrukturaMainForm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bMonostrukturaMainForm
+            // 
+            this.bMonostrukturaMainForm.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bMonostrukturaMainForm.Location = new System.Drawing.Point(12, 12);
+            this.bMonostrukturaMainForm.Name = "bMonostrukturaMainForm";
+            this.bMonostrukturaMainForm.Size = new System.Drawing.Size(260, 23);
+            this.bMonostrukturaMainForm.TabIndex = 0;
+            this.bMonostrukturaMainForm.Text = "Monostruktura Main Form";
+            this.bMonostrukturaMainForm.UseVisualStyleBackColor = true;
+            this.bMonostrukturaMainForm.Click += new System.EventHandler(this.bMonostrukturaMainForm_Click);
+            // 
+            // FrontForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.bMonostrukturaMainForm);
+            this.Name = "FrontForm";
+            this.Text = "Monostruktura";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bMonostrukturaMainForm;
+    }
+}
+
---
+++ MonostrukturaFront/FrontForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using x09_Struktura;
+
+namespace MonostrukturaFront
+{
+    public partial class FrontForm : Form
+    {
+        public FrontForm()
+        {
+            InitializeComponent();
+        }
+
+        private void bMonostrukturaMainForm_Click(object sender, EventArgs e)
+        {
+            using (StrukturaMainForm strukturaMainForm = new StrukturaMainForm())
+                strukturaMainForm.ShowDialog();
+        }
+    }
+}
