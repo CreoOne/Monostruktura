8de3e0850270d71fbb2085b0e4f42b981e2b6a47
Tomasz Czarnecki <creo.one@gmail.com>
2017-04-23 21:00:32 +0200

    Added childs classname to PartPanel for easier navigation.

--- Monostruktura/PartPanel.cs
+++ Monostruktura/PartPanel.cs
@@ -82,7 +82,7 @@ namespace Monostruktura
 
                 Button btn = new Button();
                 btn.Name = "bChild" + childIndex;
-                btn.Text = "Child " + childIndex;
+                btn.Text = "Child " + childIndex + ": " + child.GetType().Name;
                 btn.Anchor = AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right;
                 btn.Left = 3;
                 btn.Top = 3 + 26 * childIndex;
